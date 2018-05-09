5d0de0aa69c8b055d7a09628cc83ee1734f49794
ridvancakirtr@gmail.com <MuhendisTurkiye48>
2018-05-09 03:52:56 +0300

    Specialty Add

---
+++ app/src/main/java/com/example/ridvan/doctorandpatientfirebase/SpecialtyBranches.kt
@@ -0,0 +1,17 @@
+package com.example.ridvan.doctorandpatientfirebase
+
+/**
+ * Created by RIDVAN on 21/03/2018.
+ */
+class SpecialtyBranches {
+    var SpecialtyBranches: String? =null
+    var SpecialtyBranches_id: String? =null
+
+    constructor()
+
+    constructor(SpecialtyBranches: String?, SpecialtyBranches_id: String?) {
+        this.SpecialtyBranches = SpecialtyBranches
+        this.SpecialtyBranches_id = SpecialtyBranches_id
+    }
+
+}
