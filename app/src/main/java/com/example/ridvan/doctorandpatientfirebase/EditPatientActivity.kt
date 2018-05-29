8b2cf9b1bb7857c823592b2ada4e4ec15de26856
ridvancakirtr@gmail.com <MuhendisTurkiye48>
2018-05-29 19:03:35 +0300

    software improvement

--- app/src/main/java/com/example/ridvan/doctorandpatientfirebase/EditPatientActivity.kt
+++ app/src/main/java/com/example/ridvan/doctorandpatientfirebase/EditPatientActivity.kt
@@ -112,6 +112,7 @@ class EditPatientActivity : AppCompatActivity() {
                     for (singleSnapshot in p0!!.children){
                         var readUser = singleSnapshot?.getValue(DoctorDataModel::class.java)
                         editPatientDoctor.setText("DR. ${readUser!!.doctor_name_surname!!.toUpperCase()}")
+                        Log.e("doktor",readUser!!.doctor_name_surname!!.toUpperCase())
                     }
                 }
             })
